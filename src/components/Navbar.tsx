867e6b8dfe4009398640f9900b140d42e9532dcc
Matan Sweet Magic <matanglahav@gmail.com>
2025-07-07 18:19:32 -0700

    Add responsive DaisyUI navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,68 @@
 import React from "react";
 
-const Navbar: React.FC = () => (
-  <nav>
-    {/* Navbar content */}
-    <h1>Navbar</h1>
-  </nav>
-);
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar bg-base-100 border-b">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl" href="/">
+          YourBrand
+        </a>
+      </div>
+      <div className="flex-none">
+        {/* Hamburger menu for mobile */}
+        <div className="dropdown dropdown-end lg:hidden">
+          <label tabIndex={0} className="btn btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 z-50"
+          >
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/sponsorship">Sponsorship</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        {/* Horizontal menu for desktop */}
+        <ul className="menu menu-horizontal px-1 hidden lg:flex">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/sponsorship">Sponsorship</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
